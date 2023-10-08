4ad98ed290232af1eb139aefd592d0af370e70d3
Everton Buzzi <evertonbuzzi@gmail.com>
2023-10-08 13:55:48 +0100

    Initial Commit

---
+++ Daily Journal/Daily_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_JournalApp.swift
+//  Daily Journal
+//
+//  Created by Everton Buzzi on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Daily_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
